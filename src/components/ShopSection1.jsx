9cc4d12598aeae7c1157e34a4940590da8eedfa6
jude <judeorifa28@gmail.com>
2024-09-16 01:56:53 +0100

    update the view details in the shop component

--- src/components/ShopSection1.jsx
+++ src/components/ShopSection1.jsx
@@ -1,13 +1,22 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import Product from '../Json folder/Product.json';
 import { FaDownLong } from 'react-icons/fa6';
 import { cartContext } from '../Cart Folder/CartProvider';
+import { FaTimes } from 'react-icons/fa';
 
 function ShopSection1({ searchQuery }) { // Accept searchQuery as a prop
   const { addToCart } = useContext(cartContext);
 
   const [items, setItems] = useState(Product);
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedProduct,setSelectedProduct] =  useState(null)
+
+  const openPopup  = (product)=>{
+    setSelectedProduct(product)
+  }
+ const  closePopup = ()=>{
+    setSelectedProduct(null)
+  }
 
   useEffect(() => {
     if (searchQuery) {
@@ -37,6 +46,13 @@ function ShopSection1({ searchQuery }) { // Accept searchQuery as a prop
     const sortingAlphabetically = [...items].sort((a, b) => a.name.localeCompare(b.name));
     setItems(sortingAlphabetically);
   };
+
+  const modalRef = useRef()
+  const closeModal = (e)=>{
+    if(modalRef.current === e.target){
+      setSelectedProduct(false)
+    }
+  }
 
   return (
     <div className='md:px-10 lg:px-24 px-8 mt-24 font-mont'>
@@ -111,7 +127,7 @@ function ShopSection1({ searchQuery }) { // Accept searchQuery as a prop
                     <button className='py-2 px-3 rounded bg-primary' onClick={() => addToCart(item)}>
                       Add To Cart
                     </button>
-                    <button className='py-2 px-3 rounded bg-secondary'>
+                    <button className='py-2 px-3 rounded bg-secondary' onClick={()=>openPopup(item)}>
                       View Details
                     </button>
                   </div>
@@ -119,6 +135,33 @@ function ShopSection1({ searchQuery }) { // Accept searchQuery as a prop
               </div>
             ))}
           </div>
+        {selectedProduct && (
+            <div ref={modalRef}  onClick={closeModal} className='bg-primary cursor-pointer bg-opacity-30 w-full top-0 h-screen fixed inset-0 backdrop-blur-sm flex justify-center items-center'>
+            <div className='lg:max-w-[60%] bg-white py-10 px-8 z-20 rounded-md top-0 z-20 mx-auto mt-20 '>
+              <div className='flex justify-end' onClick={closePopup}>
+                <h4 className='text-2xl place-self-end'><FaTimes /></h4>
+              </div>
+              <div className='grid md:grid-cols-2 grid-cols-1 md:items-start items-start w-full gap-6'>
+                <div>
+                  <img src={selectedProduct.image} alt="" className='h-48   object-cover md:h-fit '/>
+                </div>
+                <div className='flex flex-col gap-3 tracking-normal text-sm'>
+                  <p><b>Name: </b>{selectedProduct.name}</p>
+                  <p><b>scientific name: </b>{selectedProduct.scientific_name}</p>
+                  <p><b>Origin: </b>{selectedProduct.origin}</p>
+                  <p><b>Watering: </b>{selectedProduct.watering}</p>
+                  <p><b>light: </b>{selectedProduct.light}</p>
+                  <p><b>soil: </b>{selectedProduct.soil}</p>
+                  <p><b>toxicity: </b>{selectedProduct.toxicity}</p>
+                  <p><b>ideal_temperature: </b>{selectedProduct.ideal_temperature}</p>
+                  <button className='py-2 px-3 rounded bg-primary' onClick={() => addToCart(selectedCategory)}>
+                      Add To Cart
+                    </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         </div>
       </div>
     </div>
